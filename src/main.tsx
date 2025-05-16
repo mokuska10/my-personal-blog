236fe1ca4c4d171799ffa77f355cc3a505137251
Tibazo <tibazo@indg.com>
2025-05-16 14:46:46 +0300

    Create my thoughts page
    - UI & UX
    - upload images
    - add llorem text

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Header from './components/Header/index.tsx';
-import Hero from './components/Hero/index.tsx';
+// import Header from './components/Header/index.tsx';
+// import Hero from './components/Hero/index.tsx';
+// import ReadingMotivation from './components/my-reading-motivation';
+import MyThoughtsPage from './components/my-thoughts-page';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
-    <Hero />
+    {/* <Header /> */}
+    {/* <Hero /> */}
+    {/* <ReadingMotivation /> */}
+    <MyThoughtsPage />
   </StrictMode>,
 )
