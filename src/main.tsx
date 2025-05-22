d554b6afb48937b8b41f910fb8ecd53a7567adb8
Tibazo <tibazo@indg.com>
2025-05-22 12:08:49 +0300

    Router & other pages UI modifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import Header from './components/Header/index.tsx';
-// import Hero from './components/Hero/index.tsx';
-// import ReadingMotivation from './components/my-reading-motivation';
-import MyThoughtsPage from './components/my-thoughts-page';
+import AppRouter from './router.tsx';
+
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <Header /> */}
-    {/* <Hero /> */}
-    {/* <ReadingMotivation /> */}
-    <MyThoughtsPage />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </StrictMode>,
 )
